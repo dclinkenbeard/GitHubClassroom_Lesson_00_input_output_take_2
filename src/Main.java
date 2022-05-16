74d49501a40536d5bc1d3efcb8040f66f78d8dde
daclink <drew@daclink.com>
2022-05-16 14:30:07 -0700

    solved.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Scanner scan = new Scanner(System.in);
+        System.out.println("Hello world!");
+
         System.out.println(scan.nextLine());
     }
 }
